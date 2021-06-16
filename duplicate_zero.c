e66c8beb52c36657859ee7d4c6c6f5c113b89dfd
Dragon <33679013+sadre123@users.noreply.github.com>
2021-06-16 15:05:57 +0530

    Update duplicate_zero.c

--- duplicate_zero.c
+++ duplicate_zero.c
@@ -38,7 +38,7 @@ void DuplicateZeros(int *arr, int n) {
                         for (j = n-1; j >= i+1; j--) {
                                 arr[j] = arr[j-1];
                         }
-                        arr[i+1] = 0;
+                        arr[j] = 0;
                         i = i + 1;
                 }
                 i++;
